05ec94a104a8e6dcb450b858893b8872d00ffc5f
Lr灰弹 <l2179725762@gmail.com>
2026-09-06 00:04:34 +0800

    feat：更新界面UI

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"image/color"
+	"net/url"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -18,11 +23,14 @@ type vfillLayout struct {
 func (l *vfillLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 	n := float32(len(objects))
 	total := n*l.itemHeight + (n-1)*l.spacing
-	// 按钮组在左侧垂直居中
-	y := (size.Height - total) / 2
+	// 按钮组整体靠下，最下面的按钮距底部留一点间距
+	bottomMargin := float32(24)
+	y := size.Height - total - bottomMargin
+	// 左侧留出空隙，让按钮整体稍微右移，不贴窗口左边缘
+	leftMargin := float32(16)
 	for _, o := range objects {
-		o.Resize(fyne.NewSize(size.Width, l.itemHeight))
-		o.Move(fyne.NewPos(0, y))
+		o.Resize(fyne.NewSize(size.Width-leftMargin, l.itemHeight))
+		o.Move(fyne.NewPos(leftMargin, y))
 		y += l.itemHeight + l.spacing
 	}
 }
@@ -33,9 +41,56 @@ func (l *vfillLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
 	return fyne.NewSize(l.width, h)
 }
 
+// linkText 是一个无内边距的白色下划线文字，点击后打开指定链接。
+type linkText struct {
+	widget.BaseWidget
+	text *canvas.Text
+	url  *url.URL
+}
+
+func newLinkText(label string, u *url.URL) *linkText {
+	t := canvas.NewText(label, color.Gray{Y: 0x99})
+	t.TextStyle = fyne.TextStyle{Underline: true}
+	t.TextSize = theme.CaptionTextSize()
+	return &linkText{text: t, url: u}
+}
+
+func (l *linkText) CreateRenderer() fyne.WidgetRenderer {
+	l.ExtendBaseWidget(l)
+	return &linkTextRenderer{linkText: l}
+}
+
+func (l *linkText) Tapped(*fyne.PointEvent) {
+	if l.url != nil {
+		_ = fyne.CurrentApp().OpenURL(l.url)
+	}
+}
+
+type linkTextRenderer struct {
+	linkText *linkText
+}
+
+func (r *linkTextRenderer) Layout(size fyne.Size) {
+	r.linkText.text.Resize(size)
+}
+
+func (r *linkTextRenderer) MinSize() fyne.Size {
+	return r.linkText.text.MinSize()
+}
+
+func (r *linkTextRenderer) Refresh() {
+	r.linkText.text.Refresh()
+}
+
+func (r *linkTextRenderer) Objects() []fyne.CanvasObject {
+	return []fyne.CanvasObject{r.linkText.text}
+}
+
+func (r *linkTextRenderer) Destroy() {}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("KfuPet 安装 / 升级 / 卸载工具")
+	w := a.NewWindow("KfuPetUpdate")
 	// 长方形界面
 	w.Resize(fyne.NewSize(600, 360))
 	w.SetFixedSize(true)
@@ -61,7 +116,21 @@ func main() {
 	// 右侧：预留区域，后续放宣传图、图标等
 	right := widget.NewLabel("宣传图区域（预留）")
 
-	content := container.NewBorder(nil, nil, left, nil, right)
+	// 底部：分隔线 + 版权信息（KfuPet 可点击跳转）
+	kfupetURL, _ := url.Parse("https://github.com/KfuPet")
+	copyrightText := canvas.NewText("Copyright © 2025 - 2026 ", color.Gray{Y: 0x99})
+	copyrightText.TextSize = theme.CaptionTextSize()
+	kfupetLink := newLinkText("KfuPet", kfupetURL)
+	copyrightLine := container.NewHBox(copyrightText, kfupetLink)
+	bottom := container.NewBorder(
+		widget.NewSeparator(),              // 顶部细横线，横贯窗口宽度
+		nil,                                // 底部
+		nil,                                // 左侧
+		nil,                                // 右侧
+		container.NewCenter(copyrightLine), // 版权信息居中
+	)
+
+	content := container.NewBorder(nil, bottom, left, nil, right)
 
 	w.SetContent(content)
 
